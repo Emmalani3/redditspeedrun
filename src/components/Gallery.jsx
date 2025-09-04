5e17306dec6b4feb9ac5bf043490d8ed3a67c773
EmmaLani <emma.bufalini@gmail.com>
2025-09-04 00:32:22 -0500

    test branch

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ import Tile from './Tile'
 function Carousel() {
   return (
     <div>
-      <h2>Gallery</h2>
+      <h2>Gallery!</h2>
       <p> I hold the tiles</p>
       <Tile />
       <Tile />
