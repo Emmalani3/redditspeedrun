b1164ba17bb3af7743e6a95b418c4f883499ac30
EmmaLani <emma.bufalni@gmail.com>
2025-09-03 23:50:12 -0500

    fix Gallery on App.js from Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Carousel from './components/Carousel'
 import Timer from './components/Timer'
 import Header from './components/Header'
 import DrawingArea from './components/DrawingArea'
-
+import Gallery from './components/Gallery'
 
 function App() {
 
   return (
     <>
-    <Header />
-    <Carousel />
-    <Timer />
-    <DrawingArea />
+      <Header />
+      <Gallery />
+      <Timer />
+      <DrawingArea />
     </>
   )
 }
