a82b72c90f0fdffae1b5403aa7b318c0d43849c1
EmmaLani <emma.bufalini@gmail.com>
2025-09-06 03:30:34 -0500

    fix base and remove tile

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   
-base: 'redditspeedrun',  //←Only if deploying to github not on netlify!
+base: '/redditspeedrun/',  //←Only if deploying to github not on netlify!
 plugins: [react()],
   server: {
     proxy: {
