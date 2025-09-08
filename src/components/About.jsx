57f54b6414b23934889cf682b9a68b4b4e33c44f
EmmaLani <emma.bufalni@gmail.com>
2025-09-07 19:16:04 -0500

    css and headshot  update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,58 +1,62 @@
 import React from 'react'
+import headshot from '../assets/Head-Shot-2.jpg';
+
 
 function About() {
   return (
     <>
-        <section id="support" class="center">
-            <h2>Support the Creator</h2>
-            <img
-                class="headshot"
-                src="../assets/Head-Shot-2.jpg"
-                alt="Emma Lani — programming student"
-            />
-            <p class="sub">If you enjoy this project and want to help us build more you can donate to our Ko-Fi</p>
-            <a class="kofi" href="https://ko-fi.com/emmalani" target="_blank" rel="noopener">
-                Buy me a Ko‑fi
-            </a>
-        </section>
-
-        <section id="contact">
-            <h2>Commissions & Contact</h2>
-            <p class="sub">Have an idea for a custom project or comments on this one? Send a note below and I'll get back to you.</p>
-            <div class="space"></div>
-            <form action="https://formspree.io/f/mjkwbbjn" method="POST" class="card" aria-label="Commission contact form">
-                <label>
-                Your name
-                <br/>
-                <input type="text" name="name" required placeholder="Your name" />
-                </label>
-                <label>
-                Email
-                <br/>
-                <input type="email" name="email" required placeholder="you@example.com" />
-                </label>
-                <label>
-                What would you like to make together?
-                <br />
-                <textarea name="message" rows="6" required placeholder="Tell me about your idea…"></textarea>
-                </label>
-                <button type="submit">Send request</button>
-            </form>
-        </section>
+        <div className="col2">
+            <div id="support">
+                <h2>Support the Creator</h2>
+                <img
+                    className="headshot"
+                    src={headshot}
+                    alt="Emma Lani — programming student"
+                />
+                <p className="sub">If you enjoy this project and want to help us build more you can donate to our Ko-Fi</p>
+                <a className="kofi" href="https://ko-fi.com/emmalani" target="_blank" rel="noopener">
+                    Buy me a Ko‑fi
+                </a>
+            </div>
+        
+            <div id="contact">
+                <h2>Commissions & Contact</h2>
+                <p className="sub">Have an idea for a custom project or comments on this one? Send a note below and I'll get back to you.</p>
+                <div className="space"></div>
+                <form action="https://formspree.io/f/mjkwbbjn" method="POST" className="card" aria-label="Commission contact form">
+                    <label>
+                    Your name
+                    <br/>
+                    <input type="text" name="name" required placeholder="Your name" />
+                    </label>
+                    <label>
+                    Email
+                    <br/>
+                    <input type="email" name="email" required placeholder="you@example.com" />
+                    </label>
+                    <label>
+                    What would you like to make together?
+                    <br />
+                    <textarea name="message" rows="6" required placeholder="Tell me about your idea…"></textarea>
+                    </label>
+                    <button type="submit">Send request</button>
+                </form>
+            </div>
+        </div>
 
         <section id="OtherProjects">
             <h2>Other Projects</h2>
-            <p class="sub">Emma has other ongoing projects and designs you can check out. If you like what you see take a look at these links!</p>
+            <p className="sub">Emma has other ongoing projects and designs you can check out. If you like what you see take a look at these links!</p>
             <a href="https://emmalani3.github.io/buttonGame/" alt="ButtonGame" ><button>Button Game</button></a>
-            <p class="sub">Play the button game and watch out for the get-wrecked button!</p>
+            <p className="sub">Play the button game and watch out for the get-wrecked button!</p>
             <a href="https://emmalani3.github.io/BoopTheSnoot/" alt="Boop the snoot game" ><button>Boop The snoot Game</button></a>
-            <p class="sub">These animals are waiting for you to make their day and boop ther snoots. This is a zen game
+            <p className="sub">These animals are waiting for you to make their day and boop ther snoots. This is a zen game
                 for stress relief that lets you boop as long as you want.</p>
             <a href="https://emmalani3.github.io/kelvintheweatherrock/" alt="Kelvin the weather rock" ><button>Kelvin The Weather Rock</button></a>
-            <p class="sub">Ever wanted a weatehr rock? Kelvin the rock always wanted to be a weather man so we made his 
+            <p className="sub">Ever wanted a weatehr rock? Kelvin the rock always wanted to be a weather man so we made his 
                 dreams come true. Check out Kelvin and his expressive weather reports.</p>
             <a href="https://emmalani3.github.io/homepage/" alt="Multi-Media Portfolio" ><button>Multi-Media Portfolio</button></a>
-            <p class="sub">Want to see more art by Emma? Check out our home page, one of our first website designs that 
+            <p className="sub">Want to see more art by Emma? Check out our home page, one of our first website designs that 
                 showcases different multi-media projects!</p>
 
         </section>
